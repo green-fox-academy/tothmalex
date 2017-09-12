b1fb84722ca412ff4c0f73ae01cabd9ffd4ec0c2
Alex Toth <drive_alex@hotmail.com>
2017-09-12 14:16:56 +0200

    Day02exe03

---
+++ week-02/day-02/src/AppendA.java
@@ -0,0 +1,20 @@
+public class AppendA {
+    public static void main(String[] args) {
+        String am ="kuty";
+        appendA(am);
+    }
+    public static void appendA (String input){
+        String myText=input+"a";
+        System.out.println(myText);
+    }
+}
+
+
+
+//  Create the usual class wrapper
+//  and main method on your own.
+
+// - Create a string variable named `am` and assign the value `kuty` to it
+// - Write a function called `appendA` that gets a string as an input
+//   and appends an 'a' character to its end
+// - Print the result of `appendA(am)`
--- week-02/day-02/src/Greeterfunction.java
+++ week-02/day-02/src/Greeterfunction.java
@@ -4,15 +4,8 @@ public class Greeterfunction {
         greet(al);
     }
     public static void greet(String input) {
-        System.out.println("Greetings dear, "+input);
+        String greeter="Greetings dear, " + input;
+        System.out.println(greeter);
     }
 }
 
-
-//  From now on, create the usual class wrapper
-//  and main method on your own.
-
-// - Create a string variable named `al` and assign the value `Greenfox` to it
-// - Create a function called `greet` that greets it's input parameter
-//     - Greeting is printing e.g. `Greetings dear, Greenfox`
-// - Greet `al`
