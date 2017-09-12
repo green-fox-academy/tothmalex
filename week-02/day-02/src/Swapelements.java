c68972f064319ab270ea00619d5c4ff2f61ff1f8
Alex Toth <drive_alex@hotmail.com>
2017-09-12 17:21:05 +0200

    Day02exe16

---
+++ week-02/day-02/src/Swapelements.java
@@ -0,0 +1,15 @@
+public class Swapelements {
+    public static void main(String[] args) {
+        String[] abc={"first","second","third"};
+
+        String temp = abc[0];
+        abc[2] = abc[0];
+        abc[2] = temp;
+
+        System.out.println(abc[2]);
+    }
+}
+
+// - Create an array variable named `abc`
+//   with the following content: `["first", "second", "third"]`
+// - Swap the first and the third element of `abc`
