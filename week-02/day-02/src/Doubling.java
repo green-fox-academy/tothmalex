65a46e25ad0b83b9096c265fd32af76aeb7b454b
Alex Toth <drive_alex@hotmail.com>
2017-09-12 13:59:13 +0200

    Day02exe01

---
+++ week-02/day-02/src/Doubling.java
@@ -0,0 +1,10 @@
+public class Doubling {
+    public static void main(String[] args) {
+        int ak = 123;
+        doubling(ak);
+    }
+        public static void doubling(int input) {
+            int doubleak= input*2;
+            System.out.println(doubleak);
+    }
+}
---
+++ week-02/day-02/src/writeFunction.java
@@ -0,0 +1,10 @@
+public class writeFunction {
+    public static void main(String[] args) {
+
+        for (int i=1; i<6; i++){
+            String endMessage ="End of the sentence...";
+            System.out.println("Hello! "+endMessage+" "+i);
+        }
+
+    }
+}
