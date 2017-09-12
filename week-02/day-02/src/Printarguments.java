7485d57d9dc8a23fd4c255af7c32db3301a480d7
Alex Toth <drive_alex@hotmail.com>
2017-09-12 15:49:08 +0200

    Day02exe07

--- week-02/day-02/src/Printarguments.java
+++ week-02/day-02/src/Printarguments.java
@@ -1,3 +1,18 @@
+public class Printarguments {
+    public static void main(String[] args) {
+        String[] myList ={"kutya","macska","disznó"} ;
+        System.out.println(printer(myList));
+
+    }
+
+    public static String printer(String... input) {
+        String all="";
+        for (int i = 0; i < input.length; i++) {
+            all += " "+ input[i];
+        }
+        return all;
+    }
+}
 
 //  Create the usual class wrapper
 //  and main method on your own.
--- week-02/day-02/src/Sum.java
+++ week-02/day-02/src/Sum.java
@@ -1,12 +1,12 @@
 public class Sum {
     public static void main(String[] args) {
-        int myNumber = 8;
+        int myNumber = 6;
         sum(myNumber);
     }
 
     public static void sum(int input) {
         int num = 0;
-        for (int i = 1; i <= input; i++) {
+        for (int i = 1; i < input; i++) {
             num += i;
         }
         System.out.println(num);
