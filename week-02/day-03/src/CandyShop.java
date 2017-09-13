bfddc2aae311938c8e139f3522a5c623bcc3d2ce
Alex Toth <drive_alex@hotmail.com>
2017-09-13 15:39:14 +0200

    Day03exe09

---
+++ week-02/day-03/src/CandyShop.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class CandyShop{
+    public static void main(String... args){
+        ArrayList<Object> arrayList = new ArrayList<Object>();
+        arrayList.add("Cupcake");
+        arrayList.add(2);
+        arrayList.add("Brownie");
+        arrayList.add(false);
+
+       arrayList.set(arrayList.indexOf(2),"Croissant");
+       arrayList.set(arrayList.indexOf(false),"Ice cream");
+
+        System.out.println(arrayList);
+    }
+}
+
