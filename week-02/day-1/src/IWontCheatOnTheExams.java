99369b07ba8bf63f851b30106f599aa8132b644e
Alex Toth <drive_alex@hotmail.com>
2017-09-13 17:10:25 +0200

    Day01exeWontCheat

---
+++ week-02/day-1/src/IWontCheatOnTheExams.java
@@ -0,0 +1,10 @@
+public class IWontCheatOnTheExams {
+    public static void main(String[] args) {
+        int a=100;
+
+        for (int i = 0; i<a+1 ; i++) {
+            System.out.println("I won't cheat on the exams!");
+        }
+    }
+}
+
