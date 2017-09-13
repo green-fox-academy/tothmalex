59e51aab35bf7154c9738da611a3e325871ce7da
Alex Toth <drive_alex@hotmail.com>
2017-09-13 17:18:31 +0200

    Day01exePrintEven

---
+++ week-02/day-1/src/PrintEven.java
@@ -0,0 +1,13 @@
+public class PrintEven {
+    public static void main(String[] args) {
+
+        int e = 0;
+        while (e <= 500) {
+            System.out.print(" " + e);
+            e += 2;
+        }
+    }
+}
+
+// Create a program that prints all the even numbers between 0 and 500
+
