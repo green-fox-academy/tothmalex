79a986a6ce29c9841090e22b606ab9f87dfa69e0
Alex Toth <drive_alex@hotmail.com>
2017-09-14 16:24:14 +0200

    Day01exeColors

--- week-02/day-02/src/Colors.java
+++ week-02/day-02/src/Colors.java
@@ -1,8 +1,33 @@
 public class Colors {
     public static void main(String[] args) {
 
+        String[][] colors = new String[3][5];
+
+        colors[0][0] = "lime";
+        colors[0][1] = "forest green";
+        colors[0][2] = "olive";
+        colors[0][3] = "pale green";
+        colors[0][4] = "spring green";
+
+        colors[1][0] = "orange red";
+        colors[1][1] = "red";
+        colors[1][2] = "tomato";
+
+        colors[2][0] = "orchid";
+        colors[2][1] = "violet";
+        colors[2][2] = "pink";
+        colors[2][3] = "hot pink";
+
+        for (int i = 0; i < colors.length; i++) {
+            for (int j = 0; j < colors[i].length; j++) {
+                if (colors[i][j] != null) {
+                    System.out.println(colors[i][j]);
+                }
+            }
+        }
     }
 }
+
 
 // - Create a two dimensional array
 //   which can contain the different shades of specified colors
--- week-02/day-03/src/QuoteSwap.java
+++ week-02/day-03/src/QuoteSwap.java
@@ -14,10 +14,3 @@ public class QuoteSwap{
         }
     }
 }
-
-// what I cannot create, I do not udnderstand
-// Accidentally I messed up this quote from Richard Feynman.
-// Two words are out of place
-// Your task is to fix it by swapping the right words with code
-
-// Also, print the sentence to the output with spaces in between.
