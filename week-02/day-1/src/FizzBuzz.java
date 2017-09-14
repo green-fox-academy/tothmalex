e6a60f069a854e9989211f73b44c6ef913ee4d99
Alex Toth <drive_alex@hotmail.com>
2017-09-14 10:53:27 +0200

    Day01exeFizzBuzz

--- week-02/day-1/src/FizzBuzz.java
+++ week-02/day-1/src/FizzBuzz.java
@@ -2,13 +2,13 @@ public class FizzBuzz {
     public static void main(String[] args) {
 
         for (int i = 1; i < 101; i++) {
-            if (i%3==0){
-                if (i%5==0){
+            if (i%3 == 0){
+                if (i%5 == 0){
                     System.out.println("FizzBuzz");
                 } else {
                     System.out.println("Fizz");
                 }
-            } else { if (i%5==0) {
+            } else { if (i%5 == 0) {
                     System.out.println("Buzz");
                 } else {
                     System.out.println(i);
