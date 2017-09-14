e0ae6f49ad8536832cc0fb2421ec47781c2e3598
Alex Toth <drive_alex@hotmail.com>
2017-09-14 11:13:57 +0200

    Day01exeDrawTrriangle

---
+++ week-02/day-1/src/DrawTriangle.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class DrawTriangle {
+    public static void main(String[] args) {
+
+        int numberInput;
+        String star = "*";
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Enter the number:\n ");
+        numberInput = keyboard.nextInt();
+
+        for( int i = 0; i <= numberInput; i++ ){
+            for( int j = 0; j <= i; j++ ){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// Write a program that reads a number from the standard input, then draws a
+// triangle like this:
+//
+// *
+// **
+// ***
+// ****
+//
+// The triangle should have as many lines as the number was
