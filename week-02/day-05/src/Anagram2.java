748a33b8dfe662d01a1b368c57881ac5c11f76fa
Alex Toth <drive_alex@hotmail.com>
2017-09-17 17:13:38 +0200

    Exercises

--- week-02/day-05/src/Anagram2.java
+++ week-02/day-05/src/Anagram2.java
@@ -16,11 +16,11 @@ public class Anagram2 {
 
         System.out.println(anagramCheck(wordFirst, wordSecond));
     }
-        public static boolean anagramCheck(String input1,String input2) {
+    public static boolean anagramCheck(String input1,String input2) {
 
-            if (input1.length() != input2.length()) {
-                return false;
-            } else { int value = 0;
+        if (input1.length() != input2.length()) {
+            return false;
+        } else { int value = 0;
             for (int i = 0; i < input1.length(); i++) {
                 value += ((int) input1.charAt(i)) ^ 2;
                 value -= ((int) input2.charAt(i)) ^ 2;
