c557277ff92470629b6b79603735b24877cce059
Alex Toth <drive_alex@hotmail.com>
2017-09-19 17:17:44 +0200

    Modifications

--- week-03/day-02/src/ReversedLines01.java
+++ week-03/day-02/src/ReversedLines01.java
@@ -4,7 +4,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
-public class ReversedLines01s {
+public class ReversedLines01 {
 
     public static void main(String[] args) {
         decrypt("src/reversed-lines.txt");
