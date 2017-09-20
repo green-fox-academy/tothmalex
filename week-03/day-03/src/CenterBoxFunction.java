557c62541c511957aa8a6e6d2f5a2f00c740af7f
Alex Toth <drive_alex@hotmail.com>
2017-09-20 12:04:20 +0200

    Day03ThreeRectsCentrePositioned

--- week-03/day-03/src/CenterBoxFunction.java
+++ week-03/day-03/src/CenterBoxFunction.java
@@ -7,14 +7,23 @@ import static javax.swing.JFrame.EXIT_ON_CLOSE;
 public class CenterBoxFunction {
 
     public static void mainDraw(Graphics graphics){
-      // create a square drawing function that takes 1 parameter:
-      // the square size
-      // and draws a square of that size to the center of the canvas.
-      // draw 3 squares with that function.
 
-
-
+        drawing(graphics, 150);
+        drawing(graphics, 105);
+        drawing(graphics, 50);
     }
+    public static void drawing(Graphics graphics, int size){
+
+        int x = (WIDTH-size)/2;
+        int y = (HEIGHT-size)/2;
+
+        graphics.setColor(Color.BLUE);
+        graphics.drawRect(x,y, size, size);
+    }
+    // create a square drawing function that takes 1 parameter:
+    // the square size
+    // and draws a square of that size to the center of the canvas.
+    // draw 3 squares with that function.
 
 //    Don't touch the code below
     static int WIDTH = 320;
