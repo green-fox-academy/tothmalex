9790d6895d386310853d6f5c7569ef41e6495007
Alex Toth <drive_alex@hotmail.com>
2017-09-20 17:21:09 +0200

    Day03FunctionToCenter

--- week-03/day-03/src/FunctionToCenter.java
+++ week-03/day-03/src/FunctionToCenter.java
@@ -7,14 +7,33 @@ import static javax.swing.JFrame.EXIT_ON_CLOSE;
 public class FunctionToCenter {
 
     public static void mainDraw(Graphics graphics){
-      // create a line drawing function that takes 2 parameters:
-      // the x and y coordinates of the line's starting point
-      // and draws a line from that point to the center of the canvas.
-      // fill the canvas with lines from the edges, every 20 px, to the center.
 
-
-
+        drawing(graphics, 0,0);
     }
+    public static void drawing(Graphics graphics, int x, int y) {
+
+        for (int i = x; i <= HEIGHT; i++) {
+            if (i == 0 || i == HEIGHT) {
+                for (int j = y; j < WIDTH; j += 20) {
+                    graphics.setColor(Color.BLACK);
+                    graphics.drawLine(j, i, WIDTH / 2, HEIGHT / 2);
+                }
+            }
+        }
+        for (int j = y; j <= WIDTH; j++) {
+            if (j == 0 || j == WIDTH) {
+                for (int i = x; i < HEIGHT; i += 20) {
+                    graphics.setColor(Color.BLACK);
+                    graphics.drawLine(j, i, WIDTH / 2, HEIGHT / 2);
+                }
+            }
+        }
+    }
+
+    // create a line drawing function that takes 2 parameters:
+    // the x and y coordinates of the line's starting point
+    // and draws a line from that point to the center of the canvas.
+    // fill the canvas with lines from the edges, every 20 px, to the center.
 
 //    Don't touch the code below
     static int WIDTH = 320;
