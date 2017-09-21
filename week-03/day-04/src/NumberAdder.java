5ee848c0e39b66a0efb0112d7a2e094a44c43452
Alex Toth <drive_alex@hotmail.com>
2017-09-21 11:16:13 +0200

    Day04NumberAdder

--- week-03/day-04/src/NumberAdder.java
+++ week-03/day-04/src/NumberAdder.java
@@ -1,1 +1,19 @@
+public class NumberAdder {
+
+    public static void main(String[] args) {
+
+        int n= 5;
+
+        System.out.println(countingRecursion(n));
+    }
+
+    public static int countingRecursion(int n) {
+        if (n == 1) {
+            return 1;
+        } else {
+            return n + countingRecursion(n-1);
+        }
+    }
+}
+
 // Write a recursive function that takes one parameter: n and adds numbers from 1 to n.
