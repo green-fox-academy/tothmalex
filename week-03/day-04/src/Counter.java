e69a91eed0ba4524c685750db60176a3ac0a2eef
Alex Toth <drive_alex@hotmail.com>
2017-09-21 11:05:45 +0200

    Day04Recursionexes

---
+++ week-03/day-04/src/Counter.java
@@ -0,0 +1,30 @@
+public class Counter {
+
+    public static void main(String[] args) {
+
+        int n= 5;
+
+        System.out.println(countingDown(n));
+        System.out.println(countingRecursion(n));
+    }
+    public static String countingDown(int n) {
+        String count = "";
+        for (int i = n; i  > -1 ; i--) {
+        count += i + " ";
+        }
+        return count;
+        }
+
+    public static int countingRecursion(int n) {
+        if (n == 1) {
+            return 1;
+        } else {
+            System.out.println(n);
+            return countingRecursion(n-1);
+        }
+    }
+}
+
+
+
+// Write a recursive function that takes one parameter: n and counts down from n.
