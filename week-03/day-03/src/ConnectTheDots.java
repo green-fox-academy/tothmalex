001f54e7408d9a85dfa04e47cdbc232c3d883953
Alex Toth <drive_alex@hotmail.com>
2017-09-21 10:25:10 +0200

    Day03ConnectTheDots

--- week-03/day-03/src/ConnectTheDots.java
+++ week-03/day-03/src/ConnectTheDots.java
@@ -7,16 +7,42 @@ import static javax.swing.JFrame.EXIT_ON_CLOSE;
 public class ConnectTheDots {
 
     public static void mainDraw(Graphics graphics){
-      // create a function that takes 1 parameter:
-      // an array of {x, y} points
-      // and connects them with green lines.
-      // connect these to get a box: {{10, 10}, {290,  10}, {290, 290}, {10, 290}}
-      // connect these: {{50, 100}, {70, 70}, {80, 90}, {90, 90}, {100, 70},
-      // {120, 100}, {85, 130}, {50, 100}}
 
+        int[][] points = {{10, 10}, {290,  10}, {290, 290}, {10, 290}};
+        conDot(graphics, points);
+    }
 
+    public static void conDot(Graphics graphics, int[][] points) {
+
+        int x1 = points[0][0];
+        int y1 = points[0][1];
+        int x2 = points[1][0];
+        int y2 = points[1][1];
+        int x3 = points[2][0];
+        int y3 = points[2][1];
+        int x4 = points[3][0];
+        int y4 = points[3][1];
+
+        graphics.setColor(Color.GREEN);
+        graphics.drawLine(x1, y1, x2, y2);
+
+        graphics.setColor(Color.GREEN);
+        graphics.drawLine(x2, y2, x3, y3);
+
+        graphics.setColor(Color.GREEN);
+        graphics.drawLine(x3, y3, x4, y4);
+
+        graphics.setColor(Color.GREEN);
+        graphics.drawLine(x1, y1, x4, y4);
 
     }
+    
+// create a function that takes 1 parameter:
+    // an array of {x, y} points
+    // and connects them with green lines.
+    // connect these to get a box: {{10, 10}, {290,  10}, {290, 290}, {10, 290}}
+    // connect these: {{50, 100}, {70, 70}, {80, 90}, {90, 90}, {100, 70},
+    // {120, 100}, {85, 130}, {50, 100}}
 
 //    Don't touch the code below
     static int WIDTH = 320;
