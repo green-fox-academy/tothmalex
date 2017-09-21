bb355bd78265e92e3ce7481cf26ab36e3bc32efc
Alex Toth <drive_alex@hotmail.com>
2017-09-21 15:39:17 +0200

    Day04String1

--- week-03/day-04/src/String1.java
+++ week-03/day-04/src/String1.java
@@ -1,2 +1,15 @@
+public class String1 {
+
+    public static void main(String[] args) {
+        System.out.println(string1("AlexX"));
+    }
+    public static String string1(String inputString) {
+        if (inputString.length() == 0) {
+            return "";
+        }
+        String firstChar = inputString.startsWith("x") ? "y" : inputString.substring(0, 1);
+        return firstChar + string1(inputString.substring(1));
+    }
+}
 // Given a string, compute recursively (no loops) a new string where all the
 // lowercase 'x' chars have been changed to 'y' chars.
