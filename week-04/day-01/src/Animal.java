2bc58eac8412d107b50ff7577f192c9c4e034b3c
Alex Toth <drive_alex@hotmail.com>
2017-09-25 17:16:49 +0200

    Day01Animalexe

---
+++ week-04/day-01/src/Animal.java
@@ -0,0 +1,37 @@
+public class Animal {
+   /* Create Animal class
+    Every animal has a hunger value, which is a whole number
+                    Every animal has a thirst value, which is a whole number
+                    when creating a new animal object these values are created with the default 50 value
+    Every animal can eat() which decreases their hunger by one
+    Every animal can drink() which decreases their thirst by one
+    Every animal can play() which increases both by one
+*/
+    String name;
+    int hungerValue;
+    int thirstValue;
+
+    public Animal() {
+        this("unknown",50,50);
+    }
+
+    public Animal(String name, int hungerValue, int thirstValue) {
+        this.name = name;
+        this.hungerValue = hungerValue;
+        this.thirstValue = thirstValue;
+    }
+
+    public void canEat(){
+        this.hungerValue += 1;
+    }
+
+    public void canDrink(){
+        this.thirstValue += 1;
+    }
+
+    public void canPlay(){
+        this.hungerValue += 1;
+        this.thirstValue += 1;
+    }
+
+}
