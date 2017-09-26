b5f6416faae76432b802a834c6246b0553ff2c11
Alex Toth <drive_alex@hotmail.com>
2017-09-26 15:56:04 +0200

    Day02Person

---
+++ week-04/day-02/src/Sponsor.java
@@ -0,0 +1,29 @@
+
+public class Sponsor extends Person {
+
+    String company;
+    int hiredStudents;
+
+    public void getGoal() {
+        System.out.println("Hire brilliant junior software developers.");
+    }
+
+    public void introduce() {
+        System.out.println("Hi, I'm " + name + ", a " + age + " year old " + gender + " who represents " + company + " and hired " + hiredStudents + " students so far.");
+    }
+
+    public void hire () {
+        hiredStudents++;
+    }
+
+    public Sponsor(String name, int age, String gender, String company) {
+        super(name, age, gender);
+        this.company = company;
+        this.hiredStudents = 0;
+    }
+
+    public Sponsor() {
+        this.company = "Google";
+        this.hiredStudents = 0;
+    }
+}
