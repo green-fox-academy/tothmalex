345004cb55c811f7fd2ec245066ddf920e8b8025
Alex Toth <drive_alex@hotmail.com>
2017-09-27 14:36:51 +0200

    Day03Anagramexe

---
+++ week-04/day-03/src/Anagram.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class Anagram {
+
+    /*String wordOne;
+    String wordTwo;
+
+    public Anagram(String wordOne, String wordTwo) {
+        this.wordOne = "alma";
+        this.wordTwo = "lama";
+    }*/
+
+    public boolean ifAnagram (String wordOne, String wordTwo) {
+        char[] word1 = wordOne.replaceAll("[\\s]", "").toCharArray();
+        char[] word2 = wordTwo.replaceAll("[\\s]", "").toCharArray();
+        Arrays.sort(word1);
+        Arrays.sort(word2);
+        return Arrays.equals(word1, word2);
+    }
+}
+
+
+
+   /* Write a function, that takes two strings and returns a boolean value based on if the two strings are Anagramms or not.
+        Create a test for that.*/
