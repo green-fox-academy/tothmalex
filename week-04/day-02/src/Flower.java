185c9a7881a27a6e7bc9d86739b4bcc4ec571037
Alex Toth <drive_alex@hotmail.com>
2017-10-01 16:31:13 +0200

    Day01Garden

---
+++ week-04/day-02/src/Flower.java
@@ -0,0 +1,10 @@
+package garden;
+
+public class Flowers extends Plant {
+
+    public Flower(String color) {
+        this.color = color;
+        absorbRatio = 0.75;
+        waterAmount = 0;
+    }
+}
