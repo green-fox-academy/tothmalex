50c116f36f7399618f3c4ae109fc9171a762b56f
Alex Toth <drive_alex@hotmail.com>
2017-10-03 14:22:31 +0200

    Wanderer board is done

--- week-05/day-02/src/Board.java
+++ week-05/day-02/src/Board.java
@@ -13,7 +13,7 @@ import java.awt.event.KeyListener;
             testBoxY = 300;
 
             // set the size of your draw board
-            setPreferredSize(new Dimension(720, 720));
+            setPreferredSize(new Dimension(720, 792));
             setVisible(true);
         }
 
@@ -26,39 +26,182 @@ import java.awt.event.KeyListener;
 
             int x = 0;
             int y = 0;
-
-            for (int i = 0; i < 10 ; i++) {
-                for (int j = 0; j < 10 ; j++) {
+            for (int i = 0; i < 11; i++) {
+                for (int j = 0; j < 10; j++) {
                     PositionedImage image = new PositionedImage("src/floor.png", x, y);
                     image.draw(graphics);
-                    y += 72;
+                    x += 72;
                 }
                 x = 0;
                 y += 72;
             }
-        }
 
-        // To be a KeyListener the class needs to have these 3 methods in it
-        @Override
-        public void keyTyped(KeyEvent e) {
-
-        }
-
-        @Override
-        public void keyPressed(KeyEvent e) {
-
-        }
-
-        // But actually we can use just this one for our goals here
-        @Override
-        public void keyReleased(KeyEvent e) {
-            // When the up or down keys hit, we change the position of our box
-            if (e.getKeyCode() == KeyEvent.VK_UP) {
-                testBoxY -= 100;
-            } else if(e.getKeyCode() == KeyEvent.VK_DOWN) {
-                testBoxY += 100;
+            x = 72;
+            y = 144;
+            for (int i = 2; i < 3; i++) {
+                for (int j = 1; j < 4; j++) {
+                    PositionedImage image = new PositionedImage("src/wall.png", x, y);
+                    image.draw(graphics);
+                    x += 72;
+                }
             }
-            // and redraw to have a new picture with the new coordinates
-            repaint();
+
+            x = 216;
+            y = 0;
+            for (int i = 0; i < 3; i++) {
+                for (int j = 3; j < 4; j++) {
+                    PositionedImage image = new PositionedImage("src/wall.png", x, y);
+                    image.draw(graphics);
+                    y += 72;
+                }
+            }
+// ........................
+            x = 72 * 5;
+            y = 72 * 4;
+            for (int i = 5; i < 6; i++) {
+                for (int j = 6; j < 10; j++) {
+                    PositionedImage image = new PositionedImage("src/wall.png", x, y);
+                    image.draw(graphics);
+                    x += 72;
+                }
+            }
+
+            x = 72 * 5;
+            y = 0;
+            for (int i = 0; i < 4; i++) {
+                for (int j = 5; j < 6; j++) {
+                    PositionedImage image = new PositionedImage("src/wall.png", x, y);
+                    image.draw(graphics);
+                    y += 72;
+                }
+            }
+
+            x = 72 * 8;
+            y = 72 * 5;
+            for (int i = 5; i < 9; i++) {
+                for (int j = 9; j < 10; j++) {
+                    PositionedImage image = new PositionedImage("src/wall.png", x, y);
+                    image.draw(graphics);
+                    y += 72;
+                }
+            }
+
+            //------------------- creating the blocks
+            x = 72 * 7;
+            y = 72;
+            for (int i = 1; i < 3; i++) {
+                for (int j = 7; j < 9; j++) {
+                    PositionedImage image = new PositionedImage("src/wall.png", x, y);
+                    image.draw(graphics);
+                    x += 72;
+                }
+                x = 72 * 7;
+                y += 72;
+            }
+
+            x = 72 * 5;
+            y = 72 * 6;
+            for (int i = 5; i < 7; i++) {
+                for (int j = 6; j < 8; j++) {
+                    PositionedImage image = new PositionedImage("src/wall.png", x, y);
+                    image.draw(graphics);
+                    x += 72;
+                }
+                x = 72 * 5;
+                y += 72;
+            }
+
+            //....................
+            x = 0;
+            y = 72 * 4;
+            for (int i = 4; i < 5; i++) {
+                for (int j = 0; j < 4; j++) {
+                    PositionedImage image = new PositionedImage("src/wall.png", x, y);
+                    image.draw(graphics);
+                    x += 72;
+                }
+            }
+
+            x = 72;
+            y = 72 * 5;
+            for (int i = 4; i < 6; i++) {
+                for (int j = 1; j < 2; j++) {
+                    PositionedImage image = new PositionedImage("src/wall.png", x, y);
+                    image.draw(graphics);
+                    y += 72;
+                }
+            }
+
+            x = 72 * 3;
+            y = 72 * 5;
+            for (int i = 4; i < 6; i++) {
+                for (int j = 3; j < 4 ; j++) {
+                    PositionedImage image = new PositionedImage("src/wall.png", x, y);
+                    image.draw(graphics);
+                    y += 72;
+                }
+            }
+
+            x = 72 * 5;
+            y = 72 * 9;
+            for (int i = 9; i < 10; i++) {
+                for (int j = 5; j < 7; j++) {
+                    PositionedImage image = new PositionedImage("src/wall.png", x, y);
+                    image.draw(graphics);
+                    x += 72;
+                }
+            }
+
+            x = 72;
+            y = 72 * 8;
+            for (int i = 8; i < 9; i++) {
+                for (int j = 1; j < 4; j++) {
+                    PositionedImage image = new PositionedImage("src/wall.png", x, y);
+                    image.draw(graphics);
+                    x += 72;
+                }
+            }
+
+            x = 72 * 3;
+            y = 72 * 9;
+            for (int i = 9; i < 11; i++) {
+                for (int j = 3; j < 4; j++) {
+                    PositionedImage image = new PositionedImage("src/wall.png", x, y);
+                    image.draw(graphics);
+                    y += 72;
+                }
+            }
+
+            // .... creating sinlge ones
+            PositionedImage image = new PositionedImage("src/wall.png", 72, 72 * 10);
+            image.draw(graphics);
+
+            image = new PositionedImage("src/wall.png", 72 * 5 , 72 * 10);
+            image.draw(graphics);
+
         }
-    }
+
+            // To be a KeyListener the class needs to have these 3 methods in it
+            @Override
+            public void keyTyped (KeyEvent e){
+
+            }
+
+            @Override
+            public void keyPressed (KeyEvent e){
+
+            }
+
+            // But actually we can use just this one for our goals here
+            @Override
+            public void keyReleased (KeyEvent e){
+                // When the up or down keys hit, we change the position of our box
+                if (e.getKeyCode() == KeyEvent.VK_UP) {
+                    testBoxY -= 100;
+                } else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+                    testBoxY += 100;
+                }
+                // and redraw to have a new picture with the new coordinates
+                repaint();
+            }
+        }
