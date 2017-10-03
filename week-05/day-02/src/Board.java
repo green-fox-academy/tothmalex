887ef779fc279d97c480a0dd50c741501e456bd4
Alex Toth <drive_alex@hotmail.com>
2017-10-03 12:16:19 +0200

    Wanderer-Basictiles

--- week-05/day-02/src/Board.java
+++ week-05/day-02/src/Board.java
@@ -20,27 +20,22 @@ import java.awt.event.KeyListener;
         @Override
         public void paint(Graphics graphics) {
             super.paint(graphics);
-            graphics.fillRect(testBoxX, testBoxY, 100, 100);
+            graphics.fillRect(testBoxX, testBoxY, 72, 72);
             // here you have a 720x720 canvas
             // you can create and draw an image using the class below e.g.
-            PositionedImage image = new PositionedImage("yourimage.png", 300, 300);
-            image.draw(graphics);
-        }
 
-        public static void main(String[] args) {
-            // Here is how you set up a new window and adding our board to it
-            JFrame frame = new JFrame("RPG Game");
-            Board board = new Board();
-            frame.add(board);
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setVisible(true);
-            frame.pack();
-            // Here is how you can add a key event listener
-            // The board object will be notified when hitting any key
-            // with the system calling one of the below 3 methods
-            frame.addKeyListener(board);
-            // Notice (at the top) that we can only do this
-            // because this Board class (the type of the board object) is also a KeyListener
+            int x = 0;
+            int y = 0;
+
+            for (int i = 0; i < 10 ; i++) {
+                for (int j = 0; j < 10 ; j++) {
+                    PositionedImage image = new PositionedImage("src/floor.png", x, y);
+                    image.draw(graphics);
+                    y += 72;
+                }
+                x = 0;
+                y += 72;
+            }
         }
 
         // To be a KeyListener the class needs to have these 3 methods in it
