c9e8d94c8c8dcb2ded8b1c79afd8c320f365a874
Alex Toth <drive_alex@hotmail.com>
2017-10-19 16:36:01 +0200

    Basic exam test resolved

--- week-05/day-05/test/UniqueCharsTest.java
+++ week-05/day-05/test/UniqueCharsTest.java
@@ -1,5 +1,26 @@
+
+/*
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import static org.junit.Assert.*;
 
 public class UniqueCharsTest {
 
+    ArrayList<String> testList;
+
+    @Test
+    public void uniqueCharacters() throws Exception {
+        testList = new ArrayList<>(Arrays.asList("k","m","n","c"));
+        assertEquals(testList, UniqueChars.uniqueCharacters("kemence"));
+    }
+
+    public void uniqueCharacters2() throws Exception {
+        testList = new ArrayList<>(Arrays.asList(" "));
+        assertEquals(testList, UniqueChars.uniqueCharacters(" "));
+    }
 }
+
+*/
